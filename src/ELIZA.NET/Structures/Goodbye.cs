3a0a5a0613228b9316c592d8e09e8196f51fc8db
Kris Craig <kriscraig@php.net>
2018-09-11 22:24:07 -0700

    Goodbye class.

---
+++ src/ELIZA.NET/Structures/Goodbye.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET.Structures
+{
+    public class Goodbye
+    {
+        private string message = null;
+        private string script = null;
+
+        public Goodbye(string message, string script)
+        {
+            this.message = message;
+            this.script = script;
+        }
+
+        public Goodbye() { }
+
+        public string GetMessage()
+        {
+            return message;
+        }
+
+        public void SetMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public string GetScript()
+        {
+            return script;
+        }
+
+        public void SetScript(string script)
+        {
+            this.script = script;
+        }
+    }
+}
