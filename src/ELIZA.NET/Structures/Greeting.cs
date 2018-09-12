95821896c8d2d2d32af85e33587783f894410682
Kris Craig <kriscraig@php.net>
2018-09-11 22:26:03 -0700

    Greeting class.

---
+++ src/ELIZA.NET/Structures/Greeting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET.Structures
+{
+    public class Greeting
+    {
+        private string greeting = null;
+        private string script = null;
+
+        public Greeting(string greeting, string script)
+        {
+            this.greeting = greeting;
+            this.script = script;
+        }
+
+        public Greeting() { }
+
+        public string GetGreeting()
+        {
+            return greeting;
+        }
+
+        public void SetGreeting(string greeting)
+        {
+            this.greeting = greeting;
+        }
+
+        public string GetScript()
+        {
+            return script;
+        }
+
+        public void SetScript(string script)
+        {
+            this.script = script;
+        }
+    }
+}
