2657a3587c88f4e1a0620cef9a5353a9b4f33360
Kris Craig <kriscraig@php.net>
2018-09-11 22:53:38 -0700

    Rule class and JSON library added.

---
+++ src/ELIZA.NET/Structures/Rule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET.Structures
+{
+    public class Rule
+    {
+        private string decomposition = null;
+        private List<string> reassembly = null;
+        private bool memorize = false;  // TODO - Implement this feature.  --Kris
+
+        public Rule(string decomposition, string reassembly, bool memorize = false)
+        {
+            this.decomposition = decomposition;
+            this.reassembly = JsonConvert.DeserializeObject<List<string>>(reassembly);
+            this.memorize = memorize;
+        }
+
+        public Rule(string decomposition, List<string> reassembly, bool memorize = false)
+        {
+            this.decomposition = decomposition;
+            this.reassembly = reassembly;
+            this.memorize = memorize;
+        }
+
+        public Rule() { }
+
+        public string GetDecomposition()
+        {
+            return decomposition;
+        }
+
+        public void SetDecomposition(string decomposition)
+        {
+            this.decomposition = decomposition;
+        }
+
+        public List<string> GetReassembly()
+        {
+            return reassembly;
+        }
+
+        public void SetReassembly(string reassembly)
+        {
+            this.reassembly = JsonConvert.DeserializeObject<List<string>>(reassembly);
+        }
+
+        public void SetReassembly(List<string> reassembly)
+        {
+            this.reassembly = reassembly;
+        }
+
+        public bool GetMemorize()
+        {
+            return memorize;
+        }
+
+        public void SetMemorize(bool memorize)
+        {
+            this.memorize = memorize;
+        }
+    }
+}
