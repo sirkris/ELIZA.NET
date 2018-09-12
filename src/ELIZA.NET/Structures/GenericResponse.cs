5ba1fafdb6045d071fcec37b0ab68f3ed1f51130
Kris Craig <kriscraig@php.net>
2018-09-11 22:22:15 -0700

    Generic Response class.

---
+++ src/ELIZA.NET/Structures/GenericResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET.Structures
+{
+    public class GenericResponse
+    {
+        private string response = null;
+        private string script = null;
+
+        public GenericResponse(string response, string script)
+        {
+            this.response = response;
+            this.script = script;
+        }
+
+        public GenericResponse() { }
+
+        public string GetResponse()
+        {
+            return response;
+        }
+
+        public void SetResponse(string response)
+        {
+            this.response = response;
+        }
+
+        public string GetScript()
+        {
+            return script;
+        }
+
+        public void SetScript(string script)
+        {
+            this.script = script;
+        }
+    }
+}
