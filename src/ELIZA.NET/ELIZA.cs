6541ad90dd6b16560106193461a4bbb7a80d1f18
Kris Craig <kriscraig@php.net>
2018-09-13 00:01:58 -0700

    Access directly from main class.

--- src/ELIZA.NET/ELIZA.cs
+++ src/ELIZA.NET/ELIZA.cs
@@ -26,11 +26,28 @@ namespace ELIZA.NET
             this.Session = new Session(ScriptHandler.GetScript());
         }
 
+        /// <summary>
+        /// Say something to ELIZA and get her response.
+        /// </summary>
+        /// <returns>A string representing ELIZA's response to your query.</returns>
+        public string GetResponse(string s)
+        {
+            return Session.GetResponse(s);
+        }
+
+        /// <summary>
+        /// Manually set the script handler.
+        /// </summary>
+        /// <param name="scriptHandler">A valid ScriptHandler object.</param>
         public void SetScriptHandler(ScriptHandler scriptHandler)
         {
             this.ScriptHandler = scriptHandler;
         }
 
+        /// <summary>
+        /// Manually set the session.
+        /// </summary>
+        /// <param name="session">A valid Session object.</param>
         public void SetSession(Session session)
         {
             this.Session = session;
