c4250063621fd7701283d35e76ac5a68119025cf
Kris Craig <kriscraig@php.net>
2018-09-12 22:34:20 -0700

    Gift wrapping.

---
+++ src/ELIZA.NET/ELIZA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET
+{
+    public class ELIZA
+    {
+        public ScriptHandler ScriptHandler
+        {
+            get;
+            private set;
+        }
+
+        public Session Session
+        {
+            get;
+            private set;
+        }
+
+        public ELIZA(bool autoLoad = Constants.DEFAULT_AUTOLOAD, int source = Constants.DEFAULT_SOURCE, string sourceParam = Constants.DEFAULT_SCRIPT)
+        {
+            this.ScriptHandler = new ScriptHandler(autoLoad, source, sourceParam);
+            this.Session = new Session(ScriptHandler.GetScript());
+        }
+
+        public void SetScriptHandler(ScriptHandler scriptHandler)
+        {
+            this.ScriptHandler = scriptHandler;
+        }
+
+        public void SetSession(Session session)
+        {
+            this.Session = session;
+        }
+    }
+}
---
+++ src/ELIZA.NET/Session.cs
@@ -0,0 +1,30 @@
+﻿using ELIZA.NET.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIZA.NET
+{
+    private class Session
+    {
+        public Script Script
+        {
+            get;
+            private set;
+        }
+
+        public Session(Script script)
+        {
+            this.Script = script;
+        }
+
+        public void SetScript(Script script)
+        {
+            this.Script = script;
+        }
+
+
+    }
+}
