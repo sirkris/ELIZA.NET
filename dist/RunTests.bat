a38700ed4a7530cdac264f4efce77f4b4e799d8a
Kris Craig <KrisCraig@php.net>
2018-09-23 22:48:39 -0700

    Cleanup and updated dist build.

--- dist/RunTests.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-REM Run this batch file from the command-line in the dist directory to run the tests against the bundled DOCTOR script.
-
-RunTests.exe ..\scripts\DOCTOR\tests\doctorTest.json
